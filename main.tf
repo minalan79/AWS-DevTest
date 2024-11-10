f2a33d87dd3f01df72afa76fca63ff7283530e53
minalan79 <minalan79@gmail.com>
2024-11-10 15:33:24 +0530

    Update parameters for my free tier

--- main.tf
+++ main.tf
@@ -5,24 +5,24 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20231011.1-x86_64-gp2"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-2.0.20231011.1-x86_64-gp2"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["137112412989"] # Canonical
-}
+#   owners = ["137112412989"] # Canonical
+# }
 
 resource "aws_instance" "amazon_linux" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-06b21ccaeff8cd686"
   instance_type = var.instance_type
 
   tags = {
