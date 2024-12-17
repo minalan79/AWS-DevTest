994c95bfbe2b481821224175f321c058ce5fa6bb
Minalan A S <94749642+minalan79@users.noreply.github.com>
2024-12-17 11:18:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ingress_HTTP" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_egress_All" {
   security_group_id = aws_security_group.DevServer_sg.id
-  cidr_ipv4         = -1
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = -1
   ip_protocol       = -1
   to_port           = -1
