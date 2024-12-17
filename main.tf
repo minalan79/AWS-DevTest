564aae3c65248741376ebc51ef9ff1d6ed17b6e4
Minalan A S <94749642+minalan79@users.noreply.github.com>
2024-12-17 10:45:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "DevServer_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "Allow ingress SSH" {
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_SSH" {
   security_group_id = aws_security_group.DevServer_sg.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = ["10.0.0.0/8"]
@@ -39,7 +39,7 @@ resource "aws_vpc_security_group_ingress_rule" "Allow ingress SSH" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "Allow ingress HTTP" {
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_HTTP" {
   security_group_id = aws_security_group.DevServer_sg.id
   cidr_ipv4         = ["10.0.0.0/8"]
   from_port         = 443
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "Allow ingress HTTP" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "Allow egress All" {
+resource "aws_vpc_security_group_egress_rule" "allow_egress_All" {
   security_group_id = aws_security_group.DevServer_sg.id
   cidr_ipv4         = ["10.0.0.0/8"]
   from_port         = 0
